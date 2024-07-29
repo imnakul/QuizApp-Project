1fd9de4a9e39c463b2d8b85352e2e69329235bae
Nakul Srivastava <imnakul44@gmail.com>
2024-07-29 09:29:36 +0530

    Complete Project

--- QuizUI/script.js
+++ QuizUI/script.js
@@ -1,18 +1,18 @@
-quizData = [
-    { question: "When was the first known use of the word 'quiz'?",
-        a:"1781", b:"1771", c:"1871", d:"1881" } ,
+const quizData = [
+    { question: "Q1) When was the first known use of the word 'quiz'?",
+        a:"1781", b:"1771", c:"1871", d:"1881" , correct:"b" } ,
     {
-        question: "Which built-in function can get information from the user?",
-        a:"input", b:"get", c:"print", d:"write" } ,
+        question: "Q2) Which built-in function can get information from the user?",
+        a:"input", b:"get", c:"print", d:"write", correct:"c"  } ,
     {
-        question: "Which keyword do you use to loop over a given list of elements?",
-        a:"for", b:"while", c:"each", d:"loop" } ,
+        question: "Q3) Which keyword do you use to loop over a given list of elements?",
+        a:"for", b:"while", c:"each", d:"loop", correct:"a"  } ,
     {
-        question: "What's the purpose of the built-in zip() function?" ,
+        question: "Q4) What's the purpose of the built-in zip() function?" ,
         a:"To iterate over two or more sequences at the same time",
         b: "To combine several strings into one",
         c: "To compress several files into one archive",
-        d: "To get information from the user" }
+        d: "To get information from the user", correct:"d"  }
 ]
 
 const quiz = document.getElementById('quiz')
@@ -53,9 +53,22 @@ function getSelected() {
             answer = answerEl.id;
         }
     })
+    return answer;
 }
 
 submitBtn.addEventListener('click', () => {
     const answer  = getSelected()
-    console.log(answer)
+     if(answer){
+         if( answer === quizData[currentQuiz].correct) {
+             score++;
+         }
+         currentQuiz++;
+         if(currentQuiz < quizData.length) {
+             loadQuiz()
+         }
+         else {
+             quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correctly!</h2>
+                 <button onclick="location.reload()">Reload</button>`;
+         }
+     }
 } )
